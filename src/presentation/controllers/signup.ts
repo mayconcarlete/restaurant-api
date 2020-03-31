7ae2759e8f47b18311f3e0e32589dca5a5eeedae
maycon <maycon.carlete@hotmail.com>
2020-03-31 15:04:47 -0300

    refactor: organize code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -12,11 +12,13 @@ export class SignUpController implements Controller {
 
   handle (httpRequest: HttpRequest): HttpReponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
     }
+
     const isValid = this.emailValidator.isValid(httpRequest.body.email)
     if (!isValid) {
       return badRequest(new MissingParamError('email'))
