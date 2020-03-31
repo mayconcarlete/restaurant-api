8ebd3c5d877294ccba6200e6283eb95b892121b8
maycon <maycon.carlete@hotmail.com>
2020-03-31 09:19:55 -0300

    test: ensure SignUpController return 400 if passworde confimation are not provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -27,7 +27,7 @@ describe('Test SignUp controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
-  test('Should return 400 if email are not provided', () => {
+  test('Should return 400 if password are not provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
@@ -39,5 +39,18 @@ describe('Test SignUp controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
+  test('Should return 400 if password confirmation are not provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_password@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
   })
 })
